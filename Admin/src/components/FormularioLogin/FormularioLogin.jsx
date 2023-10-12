32f2d2b25c1fd25d099e1d26459293c3ab32e3cf
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-10-12 23:30:09 +0000

    🆕️ : Add CORS

--- Admin/src/components/FormularioLogin/FormularioLogin.jsx
+++ Admin/src/components/FormularioLogin/FormularioLogin.jsx
@@ -3,17 +3,21 @@ import { useContexto } from "../../context/useContexto";
 import { Container, Button, Form, Row, Col } from "react-bootstrap";
 
 function FormularioLogin() {
-  const { setIsLoggedin } = useContexto();
-  const handleSubmit = (event) => {
+  const { loginAadmin } = useContexto();
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    setIsLoggedin(true);
+    const form = event.target;
+    const email = form.elements["Login.email"].value;
+    const senha = form.elements["Login.senha"].value;
+    console.log(email, senha);
+    await loginAadmin(email, senha);
   };
 
   return (
-    <Container style={{border : "2px #546469 solid"}}>
-      <Row style={{ width : "100uvh"}}>
-        <Col md={6} >
-          <Form  onSubmit={handleSubmit}  style={{ width : "100uvh"}} >
+    <Container style={{ border: "2px #546469 solid" }}>
+      <Row style={{ width: "100uvh" }}>
+        <Col md={6}>
+          <Form onSubmit={handleSubmit} style={{ width: "100uvh" }}>
             <Form.Text>Login</Form.Text>
             <Form.Group className="mb-3" controlId="Login.email">
               <Form.Label>Email </Form.Label>
--- Admin/src/context/ContextProvider.jsx
+++ Admin/src/context/ContextProvider.jsx
@@ -4,12 +4,60 @@ import PropTypes from "prop-types";
 // import axios from "axios";
 export const appContext = createContext();
 
+import axios from "axios";
+
 export function ContextProvider({ children }) {
   const [isLoggedin, setIsLoggedin] = useState(false);
+  const BASEURL = "http://localhost:3333";
+  const ENDPOINTLOGIN = "/api/admin/login";
+
+  //função para validar senha
+  function validaSenha(senha) {
+    // este regex e para validar senha com pelo menos 8 caracteres, uma letra maiúscula e uma minuscula
+    const regex = /^(?=.*[A-Z])(?=.*[0-9])(?=.*\W)[a-zA-Z0-9\W]{8,}$/;
+
+    if (!regex.test(senha)) {
+      alert(
+        "Senha incorreta, verificar se  possui 8 ou mais caracteres e se há pelo menos uma letra (maiúscula ou minúscula) na senha."
+      );
+    }
+    return regex.test(senha);
+  }
+  //funçao para validar email
+  function validaEmail(email) {
+    // este regex e para validar email com domínio .com, .br, .net, etc
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!regex.test(email)) {
+      alert(
+        "Email incorreto, verificar se e um e-mail valido, ex: name@example.com"
+      );
+    }
+    return regex.test(email);
+  }
+
+  const loginAadmin = async (email, senha) => {
+    if (!validaEmail(email)) {
+      return;
+    }
+    if (!validaSenha(senha)) {
+      return;
+    }
+    try {
+      const response = await axios.post(BASEURL + ENDPOINTLOGIN, {
+        email,
+        senha,
+      });
+
+      localStorage.setItem("token", response.data.token);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const value = {
     isLoggedin,
     setIsLoggedin,
+    loginAadmin,
   };
 
   return <appContext.Provider value={value}>{children}</appContext.Provider>;
