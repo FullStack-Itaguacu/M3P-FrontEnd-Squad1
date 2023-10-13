470029d1682167c5a1661ce6e45698da0a31c2c5
adrianovolter <adriano.j.volter@hotmail.com>
2023-10-13 00:30:11 -0300

    feat: componente dashboard conectando a api

--- Admin/src/components/Dashboard/Dashboard.jsx
+++ Admin/src/components/Dashboard/Dashboard.jsx
@@ -1,28 +1,44 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Container, Card } from "react-bootstrap";
+//import { useHistory } from "react-router-dom";
+import {} from "react";
 
 function Dashboard() {
-  const [salesData, setSalesData] = useState({ totalSales: 0, totalAmount: 0 });
+  const [salesData, setSalesData] = useState({ totalSales: null, totalAmount: null });
+  const [sales, setSales] = useState([]);
+
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("http://localhost:3000/api/sales/dashboard/admin", {
           headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            Authorization: `${token}`,
           },
         });
-        const { totalSales, totalAmount } = response.data;
+        const { totalSales, totalAmount } = response.data.dados; // Certifique-se de usar a estrutur
         setSalesData({ totalSales, totalAmount });
       } catch (error) {
         console.error("Error fetching data:", error);
+        setSalesData({ totalSales: 0, totalAmount: 0 });
       }
     };
 
     fetchData();
-  }, []); // Empty dependency array means this effect will run once after the initial render
-
+  }, [token]); 
+  // Se os dados ainda estão sendo carregados, você pode mostrar uma mensagem de carregamento
+  if (salesData.totalSales === null || salesData.totalAmount === null) {
+    return <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "80vh",
+      }}
+    >Carregando...</div>;
+  }
   return (
     <Container className="mt-2 ">
       <Card>
