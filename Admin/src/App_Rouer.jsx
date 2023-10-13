c80bbd6bc4276b57b42379dccf0ce1bc29317c97
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-10-13 18:04:15 +0000

    💥️ Feat : Criaçao de arquivos para trabalhar com rotas.
      Ainda nao utilizando esperando os PR dos colegas

---
+++ Admin/src/App_Rouer.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { useContexto } from "./context/useContexto";
+
+import Header from "./components/Header/Header";
+import Login from "./pages/Login/Login";
+import Error from "./pages/Error/NotFound";
+
+function App() {
+  const { isLoggedin } = useContexto();
+
+  return <>{isLoggedin ?  
+  <BrowserRouter>
+      <Header >
+      <Routes>
+        {/* Rotas da aplicaçao quando usuario esta logado */}
+        <Route path="/" element={<h1>HOME</h1>} />
+        <Route path="examṕle" element={<h1>EXAMPLE</h1>} />
+
+        {/*Rota de erro quando nao existe a rota */}
+        <Route path="*" element={<Error />} />
+      </Routes>
+      </Header>
+  </BrowserRouter> :  
+  <BrowserRouter>
+      <Routes>
+        {/* Rotas da aplicaçao quando usuario nao esta logado */}
+        <Route path="/" element={<Login />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+  </BrowserRouter>}</>;
+}
+
+export default App;
---
+++ Admin/src/components/Header/Header_Router.jsx
@@ -0,0 +1,68 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <>
+      <Navbar expand={"lg"} className="bg-body-tertiary mb-3">
+        <Container fluid>
+          <Navbar.Brand href="#">Admin</Navbar.Brand>
+          <Navbar.Toggle aria-controls={`offcanvasNavbar-false`} />
+          <Navbar.Offcanvas
+            id={`offcanvasNavbar-expand`}
+            aria-labelledby={`offcanvasNavbarLabel-expand`}
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id={`offcanvasNavbarLabel-expand`}>
+                Admin
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link" to="/dasboard">
+                  Dashboard
+                </Link>
+                <NavDropdown
+                  title="Mais opções"
+                  id={`offcanvasNavbarDropdown-expand`}
+                >
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                  <Link className="nav-link" to="/dasboard">
+                    Dashboard
+                  </Link>
+                  <NavDropdown.Divider />
+                  <Link className="nav-link" to="/">Home</Link>
+                  <Link className="nav-link" to="/dasboard">Dashboard</Link>                
+                  </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Item Buscado"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Buscar</Button>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
