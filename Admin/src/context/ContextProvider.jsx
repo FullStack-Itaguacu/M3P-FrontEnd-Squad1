01e5437f9620e97f48cc2734411047c162e01199
adrianovolter <adriano.j.volter@hotmail.com>
2023-10-13 22:43:19 -0300

    feat: task5 cadastro de usuario / criado metodo post no context

--- Admin/src/context/ContextProvider.jsx
+++ Admin/src/context/ContextProvider.jsx
@@ -1,16 +1,32 @@
 import { createContext } from "react";
 import { useState } from "react";
 import PropTypes from "prop-types";
-// import axios from "axios";
 export const appContext = createContext();
-
 import axios from "axios";
 
 export function ContextProvider({ children }) {
   const [isLoggedin, setIsLoggedin] = useState(false);
+  const [cpf, setCpf] = useState("");
+  const [birth_date, setD] = useState("");
+  const [full_name, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [password, setPassword] = useState("");
+  const [type_user, setTypeUser] = useState("");
+
+  const [cep, setCep] = useState("");
+  const [street, setStreet] = useState("");
+  const [number_street, setNumberStreet] = useState("");
+  const [neighborhood, setNeighborhood] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [complement, setComplement] = useState("");
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
 
   const BASEURL = "http://localhost:3000";
   const ENDPOINTLOGIN = "/api/user/admin/login";
+  const ENDPOINTCADASTRO = "/api/user/admin/signup";
 
   //função para validar senha
   function validaSenha(senha) {
@@ -71,10 +87,110 @@ export function ContextProvider({ children }) {
       });
   };
 
+  // função para cadastrar usuario no banco de dados
+  async function handleCadastrarUsuario(event) {
+    if (!validaEmail(email)) {
+      return;
+    }
+    if (!validaSenha(password)) {
+      return;
+    }
+    event.preventDefault();
+
+    const token = localStorage.getItem("token");
+    try {
+      const response = await axios.post(
+        BASEURL + ENDPOINTCADASTRO,
+        {
+          user: {
+            full_name,
+            cpf,
+            birth_date,
+            email,
+            phone,
+            password,
+            type_user,
+          },
+          address: [
+            {
+              zip: cep,
+              street,
+              number_street,
+              neighborhood,
+              city,
+              state,
+              complement,
+              lat,
+              long,
+            },
+          ],
+        },
+        {
+          headers: {
+            Authorization: `${token}`,
+          },
+        }
+      );
+      console.log(response.data);
+      alert(`Usuário cadastrado com sucesso!`);
+    } catch (error) {
+      console.error(error.response.data);
+      alert(`Erro ao cadastrar usuário: ${error.response.data.message} `);
+    }
+  }
+
+  // function handleViaCep(cepCode) {
+  //   axios.get(`https://viacep.com.br/ws/${cepCode}/json/`).then((response) => {
+  //     console.log(response.data);
+  //     const { logradouro, bairro, localidade, uf } = response.data;
+  //     setStreet(logradouro);
+  //     setNeighborhood(bairro);
+  //     setCity(localidade);
+  //     setState(uf);
+  //   });
+  // }
+
   const value = {
     isLoggedin,
     setIsLoggedin,
     loginAadmin,
+    // handleViaCep,
+    validaEmail,
+    validaSenha,
+    handleCadastrarUsuario,
+    cpf,
+    setCpf,
+    birth_date,
+    setD,
+
+    full_name,
+    setFullName,
+    email,
+    setEmail,
+    phone,
+    setPhone,
+    password,
+    setPassword,
+    type_user,
+    setTypeUser,
+    cep,
+    setCep,
+    street,
+    setStreet,
+    number_street,
+    setNumberStreet,
+    neighborhood,
+    setNeighborhood,
+    city,
+    setCity,
+    state,
+    setState,
+    complement,
+    setComplement,
+    lat,
+    setLat,
+    long,
+    setLong,
   };
 
   return <appContext.Provider value={value}>{children}</appContext.Provider>;
