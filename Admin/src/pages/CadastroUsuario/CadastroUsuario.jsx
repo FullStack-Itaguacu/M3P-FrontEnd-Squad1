0c932005ffc83d07bd08c27afa85d5aeb43b4f36
adrianovolter <adriano.j.volter@hotmail.com>
2023-10-13 22:54:21 -0300

    feat: task5 cadastro de usuario / criado componente page

---
+++ Admin/src/pages/CadastroUsuario/CadastroUsuario.jsx
@@ -0,0 +1,11 @@
+import FormularioCadastroUsuario from "../../components/FormularioCadastroUsuario/FormularioCadastroUsuario";
+
+const CadastroUsuario = () => {
+    return (
+        <div>
+        <FormularioCadastroUsuario />
+        </div>
+    );
+    }
+
+export default CadastroUsuario;
