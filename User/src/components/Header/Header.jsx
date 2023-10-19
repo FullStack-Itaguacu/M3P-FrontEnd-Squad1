e14bd477f42d32800b36bf030ea3d894901cf6cd
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2023-10-19 03:18:37 +0000

    Merge developer .. resoluçao de conflitos

--- User/src/components/Header/Header.jsx
+++ User/src/components/Header/Header.jsx
@@ -1,24 +1,10 @@
-import Button from "react-bootstrap/Button";
 import { useState, useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Offcanvas from "react-bootstrap/Offcanvas";
-<<<<<<< HEAD
+import { Offcanvas, Navbar, Nav, Container } from "react-bootstrap";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
-import styles from "./Header.module.css";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { useContexto } from "../../context/useContexto";
-=======
-import styles from "./Header.module.css"
-import PropTypes from 'prop-types';
-import { Link } from "react-router-dom";
-import "bootstrap-icons/font/bootstrap-icons.css"
->>>>>>> developer
+import styles from "./Header.module.css";
+import PropTypes from "prop-types";
 
 function Header({ children, onLogout }) {
   const [userEmail, setUserEmail] = useState("");
@@ -61,20 +47,13 @@ function Header({ children, onLogout }) {
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-<<<<<<< HEAD
               <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Link
-                  className={`nav-link text-light ${styles.navbarLink}`}
-                  to="/minhas-compras"
-                >
-=======
-              <Nav className="justify-content-end flex-grow-1 pe-3">   
+               
               <Link className={`nav-link text-light ${styles.navbarLink}`} to="/">
                   <i className="bi bi-bag"></i>
                   Produtos
                 </Link>               
                 <Link className={`nav-link text-light ${styles.navbarLink}`} to="/minhas-compras">
->>>>>>> developer
                   <i className="bi bi-bag"></i>
                   Minhas Compras
                 </Link>
