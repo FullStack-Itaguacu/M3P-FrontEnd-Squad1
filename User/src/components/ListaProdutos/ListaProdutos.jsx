d2a46ea0809a8d08604c002eaf156c06a29dd506
adrianovolter <adriano.j.volter@hotmail.com>
2023-10-18 22:12:40 -0300

    feat: task-13-SE adicionando estilo no componente

--- User/src/components/ListaProdutos/ListaProdutos.jsx
+++ User/src/components/ListaProdutos/ListaProdutos.jsx
@@ -87,11 +87,8 @@ function ListagemProdutos() {
     <>
       <h1 className="p-3">Medicamentos</h1>
 
-      <Container
-        fluid
-        className=" m-2 p-2  border border-2 rounded-3 accordion"
-      >
-        <Row className="align-items-center ">
+      <Container fluid className="m-2 p-2 border border-2 rounded-3 accordion">
+        <Row className="align-items-center mb-3">
           <Col md={3}>
             <Form.Control
               type="text"
@@ -130,8 +127,8 @@ function ListagemProdutos() {
               <option value="30">30</option>
             </Form.Control>
           </Col>
-          <Col md={3}>
-            <Pagination>
+          <Col md={3} className="">
+            <Pagination className="mt-3">
               <Pagination.First onClick={() => setPage(1)} />
               <Pagination.Prev onClick={handleBack} />
               <Pagination.Item>{page}</Pagination.Item>
@@ -143,52 +140,54 @@ function ListagemProdutos() {
           </Col>
         </Row>
 
-        <Row className="align-items-center justify-content-center">
+        <Row xs={1} md={3} lg={4} className="g-4">
           {produtos.length > 0 &&
             produtos.map((produto) => (
-              <Card as={Col} md={2} className="p-3 m-2 h-100 " key={produto.id}>
-                <Card.Body>
-                  <Card.Title>{produto.name}</Card.Title>
-                  <Card.Img
-                    variant="top"
-                    src={produto.image_link}
-                    style={{ width: "100%" }}
-                  />
-                  <Card.Text>
-                    <p>Preço unitário: R$ {produto.unit_price}</p>
-                    <p>Estoque: {produto.total_stock}</p>
-                  </Card.Text>
-                </Card.Body>
+              <Col key={produto.id}>
+               <Card className="h-100">
+                  <Card.Body>
+                    <Card.Title>{produto.name}</Card.Title>
+                    <Card.Img
+                      variant="top"
+                      src={produto.image_link}
+                      style={{ width: "100%" }}
+                    />
+                    <Card.Text>
+                      <p>Preço unitário: R$ {produto.unit_price}</p>
+                      <p>Estoque: {produto.total_stock}</p>
+                    </Card.Text>
+                  </Card.Body>
 
-                <Card.Footer className="mx-1">
-                  <Form.Group controlId={`quantidade-${produto.id}`}>
-                    <Form.Label>Quantidade</Form.Label>
-                    <div className="d-flex">
-                      <Button
-                        className="btn btn-secondary m-1 hover link-opacity-75-hover"
-                        onClick={() => handleDecrement(produto.id)}
-                      >
-                        -
-                      </Button>
-                      <div className="mx-1 m-1 p-2">
-                        {quantidades[produto.id] || 0}
+                  <Card.Footer className="mx-1">
+                    <Form.Group controlId={`quantidade-${produto.id}`}>
+                      <Form.Label className="mb-2">Quantidade</Form.Label>
+                      <div className="d-flex align-items-center">
+                        <Button
+                          className="btn btn-secondary btn-sm me-1"
+                          onClick={() => handleDecrement(produto.id)}
+                        >
+                          -
+                        </Button>
+                        <div className="mx-1 my-1 p-2">
+                          {quantidades[produto.id] || 0}
+                        </div>
+                        <Button
+                          className="btn btn-warning btn-sm me-1"
+                          onClick={() => handleIncrement(produto.id)}
+                        >
+                          +
+                        </Button>
                       </div>
-                      <Button
-                        className="btn btn-warning m-1"
-                        onClick={() => handleIncrement(produto.id)}
-                      >
-                        +
-                      </Button>
-                    </div>
-                  </Form.Group>
-                </Card.Footer>
-                <button
-                  className="btn  bg-danger text-light h-auto w-100 hover link-underline-dark-hover"
-                  onClick={() => adicionarAoCarrinho(produto)}
-                >
-                  Adicionar ao Carrinho
-                </button>
-              </Card>
+                    </Form.Group>
+                  </Card.Footer>
+                  <button
+                    className="btn bg-danger text-light h-auto w-100 mt-1 mb-2 "
+                    onClick={() => adicionarAoCarrinho(produto)}
+                  >
+                    Adicionar ao Carrinho
+                  </button>
+                </Card>
+              </Col>
             ))}
         </Row>
         <Col md={3}>
