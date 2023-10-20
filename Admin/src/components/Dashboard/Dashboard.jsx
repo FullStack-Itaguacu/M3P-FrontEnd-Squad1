65aad59619510d6fb8a10b5109f0ad9f515a7f1e
filipebratti <filipebratti@hotmail.com>
2023-10-19 23:12:18 -0300

    update background color

--- Admin/src/components/Dashboard/Dashboard.jsx
+++ Admin/src/components/Dashboard/Dashboard.jsx
@@ -31,15 +31,15 @@ function Dashboard() {
   }, [token]); 
   
   return (    
-    <div className="">
+    <div className="pb-5" style={{backgroundColor: "rgb(214, 229, 243)"}} >
       <h1 className="text-center text-black pt-4">Bem-vindo ao Sistema Express Pharmacy</h1>
       <h4 className="text-center text-black p-4">Administrador, aqui você tem acesso ao Gerenciamento de Usuários e Medicamentos.</h4>
       <p className="text-center text-black">Abaixo você pode acompanhar um resumo dos seus resultados até agora:</p>
       <Stack direction="horizontal" gap={3} className="d-flex justify-content-center">
         <Col md={4}>      
-          <Card className="p-4" style={{border: "3px blue solid", backgroundColor: "rgb(3, 193, 25)" }}>
+          <Card className="p-4" style={{border: "3px hsl(216, 97%, 53%) solid", backgroundColor: "rgb(2, 141, 18)" }}>
             <Card.Body>
-              <i className="bi bi-cash-stack text-light d-flex justify-content-center p-3 "></i>
+              <i className="bi bi-cash-stack text-light d-flex justify-content-center p-3"></i>
               <Card.Title className="text-center text-white">Valor Total de Vendas</Card.Title>
               <Card.Text className="text-center text-white">{`R$ ${salesData.totalSales}`}</Card.Text>
             </Card.Body>
@@ -47,7 +47,7 @@ function Dashboard() {
         </Col>
 
         <Col md={4}>
-        <Card className="p-4" style={{border: "3px blue solid", backgroundColor: "rgb(3, 193, 25)" }}>
+        <Card className="p-4" style={{border: "3px hsl(216, 97%, 53%) solid", backgroundColor: "rgb(2, 141, 18)" }}>
           <Card.Body>
             <i className="bi bi-box-seam text-light d-flex justify-content-center p-3"></i>
             <Card.Title className="text-center text-light">Qtd Produtos Vendidos</Card.Title>
