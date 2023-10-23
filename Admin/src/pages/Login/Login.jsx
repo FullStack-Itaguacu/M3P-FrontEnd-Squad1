1ff251c471f0cb56e4092e6aeaac8fc49ecf7c3d
filipebratti <filipebratti@hotmail.com>
2023-10-23 08:42:26 -0300

    add login header

--- Admin/src/pages/Login/Login.jsx
+++ Admin/src/pages/Login/Login.jsx
@@ -1,21 +1,25 @@
 import FormularioLogin from "../../components/FormularioLogin/FormularioLogin";
 import { Row, Col, Container, Image } from "react-bootstrap";
+import LoginHeader from "../../components/LoginHeader/LoginHeader";
 import styles from "./Login.module.css";
 
 function Login() {
   return (
-    <div className={styles.loginStyle}>
-    <Container className="justify-content-md-center">
-      <Row style={{ height: "80vh" }} className="align-items-center">
-        <Col md={4} className="mx-auto align-itens-center">
-          <Image className={styles.imgStyle} src={"/logo.png"} fluid />
-        </Col>
-        <Col md={5} className="mx-auto align-items-center">
-          <FormularioLogin  />
-        </Col>
-      </Row>
-    </Container>
-    </div>
+    <>
+      <LoginHeader /> 
+      <div className={styles.loginStyle}>
+        <Container className="justify-content-md-center">
+          <Row style={{ height: "80vh" }} className="align-items-center">
+            <Col md={4} className="mx-auto align-itens-center">
+              <Image className={styles.imgStyle} src={"/logo.png"} fluid />
+            </Col>
+            <Col md={5} className="mx-auto align-items-center">
+              <FormularioLogin  />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
   );
 }
 
