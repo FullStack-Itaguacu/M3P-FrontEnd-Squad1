5997bd50380262a8686fca1336145aab01347c63
filipebratti <filipebratti@hotmail.com>
2023-10-23 14:44:47 -0300

    update background image login page

--- Admin/src/pages/Login/Login.jsx
+++ Admin/src/pages/Login/Login.jsx
@@ -4,9 +4,17 @@ import styles from "./Login.module.css";
 
 function Login() {
   return (
-    <>             
-      <Container className={styles.loginStyle}>
-        <Row className="align-items-center justify-content-center">
+    <>
+      <Container className={styles.loginStyle}> 
+        <Row>
+          <Col className={styles.leftImage} xs={6}>
+            {/* Conteúdo do lado esquerdo */}
+          </Col>
+          <Col className={styles.rightImage} xs={6}>
+            {/* Conteúdo do lado direito */}
+          </Col>
+        </Row>                 
+        <Row className={`align-items-center justify-content-center ${styles.loginRow}`}>          
           {/* <Col md={5} className="mx-auto d-flex align-items-center">
             <Image className={styles.imgStyle} src={"/logo.png"} style={{ height: '310px' }} />
           </Col> */}
