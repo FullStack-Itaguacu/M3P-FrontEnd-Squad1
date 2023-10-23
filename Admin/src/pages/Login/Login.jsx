cfe31dfde7f15b6e8c63080bab242dc78d60221d
filipebratti <filipebratti@hotmail.com>
2023-10-23 08:58:00 -0300

    fix css cards

--- Admin/src/pages/Login/Login.jsx
+++ Admin/src/pages/Login/Login.jsx
@@ -1,24 +1,22 @@
-import FormularioLogin from "../../components/FormularioLogin/FormularioLogin";
 import { Row, Col, Container, Image } from "react-bootstrap";
+import FormularioLogin from "../../components/FormularioLogin/FormularioLogin";
 import LoginHeader from "../../components/LoginHeader/LoginHeader";
 import styles from "./Login.module.css";
 
 function Login() {
   return (
     <>
-      <LoginHeader /> 
-      <div className={styles.loginStyle}>
-        <Container className="justify-content-md-center">
-          <Row style={{ height: "80vh" }} className="align-items-center">
-            <Col md={4} className="mx-auto align-itens-center">
-              <Image className={styles.imgStyle} src={"/logo.png"} fluid />
+      <LoginHeader />       
+        <Container className={styles.loginStyle}>
+          <Row className="align-items-center justify-content-center">
+            <Col md={5} className="mx-auto d-flex align-items-center">
+              <Image className={styles.imgStyle} src={"/logo.png"} style={{ height: '310px' }} />
             </Col>
-            <Col md={5} className="mx-auto align-items-center">
-              <FormularioLogin  />
+            <Col md={5} className="mx-auto d-flex align-items-center" style={{ height: '500px' }}>
+              <FormularioLogin />
             </Col>
           </Row>
-        </Container>
-      </div>
+        </Container>      
     </>
   );
 }
