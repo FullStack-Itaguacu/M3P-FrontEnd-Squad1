15bc278dcd1ce28d51b3caea36ac1c434ce28542
filipebratti <filipebratti@hotmail.com>
2023-10-26 14:03:27 -0300

    update page not found

--- User/src/pages/Error/NotFound.jsx
+++ User/src/pages/Error/NotFound.jsx
@@ -1,8 +1,30 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom"; // Importe useNavigate
+import styles from "./NotFound.module.css";
+
 function NotFound() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/"); 
+  };
+
   return (
-    <div>
-      <h1>404 Not Found</h1>
-    </div>
+    <Container className={styles.notFoundStyle}>
+      <h3 className="text-center pt-5">Página não encontrada, clique no botão e faça Login novamente:</h3>
+      <div className="d-flex justify-content-center mt-5">
+        <Button          
+          variant="primary"
+          onClick={handleClick}
+        >
+          Voltar para o Login
+        </Button>
+      </div>
+      <Row>
+        <Col className={styles.centerImage}></Col>
+      </Row>
+    </Container>
   );
 }
+
 export default NotFound;
